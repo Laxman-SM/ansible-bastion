b8f0f62bc60b279ee2faaf6b029961ab02729641
Jaroslav Libak <jaroslav.libak@hp.com>
2015-09-01 17:18:19 +0200

    added provisioning of machine for docker registry/mirror

--- filter_plugins/get_subnets.py
+++ filter_plugins/get_subnets.py
@@ -10,6 +10,15 @@ def get_subnets(value, tag_key, tag_value, return_key='id'):
 
     return subnets
 
+def get_subnets_full(value, tag_key, tag_value):
+    # return subnets that match
+    subnets = []
+    for item in value:
+      for key, value in item['resource_tags'].iteritems():
+        if key == tag_key and value == tag_value:
+          subnets.append(item)
+
+    return subnets
 
 class FilterModule(object):
     ''' Ansible core jinja2 filters '''
@@ -17,4 +26,5 @@ class FilterModule(object):
     def filters(self):
         return {
             'get_subnets': get_subnets,
+            'get_subnets_full': get_subnets_full,
         }
