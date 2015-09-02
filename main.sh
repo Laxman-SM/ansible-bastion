f6394301b184e68d15a9b0856918964d63251ca4
Jaroslav Libak <jaroslav.libak@hp.com>
2015-09-02 17:31:09 +0200

    added NAT to bastion env to allow internet access to instances with private IP only

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export EC2_INI_PATH=inventory/ec2.ini
 # create bastion VPC and autoscale groups
 # && setup bastion machines: separate playbook requires to refresh dynamic EC2 inventory
 ansible-playbook --extra-vars "environ=$1" main_setup.yaml \
