9e0d7ad016564d1f0161562b10e2f58079908d3b
Alexei Ledenev <alexei.ledenev@hp.com>
2015-10-25 17:28:09 +0200

    replace base64 linux version with cross platfomr 'openssl base64 encoding'

--- main.sh
+++ main.sh
@@ -2,5 +2,5 @@
 export EC2_INI_PATH=inventory/ec2.ini
 # create bastion VPC and autoscale groups
 # && setup bastion machines: separate playbook requires to refresh dynamic EC2 inventory
-ansible-playbook --extra-vars "environ=$1" main_setup.yaml \
-&& ansible-playbook --extra-vars "environ=$1" main_config.yaml
+ansible-playbook --extra-vars "environ=$1" main_setup.yaml $2 \
+&& ansible-playbook --extra-vars "environ=$1" main_config.yaml $2
