5e0d93819cff612426562e6fe7d05ff7e8a80c06
Alexei Ledenev <alexei.ledenev@hp.com>
2015-10-27 10:28:51 +0200

    command line parameters for specifying environment, domain and verbose mode

--- clean.sh
+++ clean.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 export EC2_INI_PATH=inventory/ec2_priv.ini
-ansible-playbook --extra-vars "environ=$1" clean.yaml $2
+
+# read input parameters
+vflag=""
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -v) vflag="-vvvv";;
+    -e) env="$2"; shift;;
+    -h)
+        echo >&2 "usage: $0 -e environment [-v]"
+        exit 1;;
+     *) break;; # terminate while loop
+  esac
+  shift
+done
+
+ansible-playbook --extra-vars "environ=$env" clean.yaml $vflag
--- main.sh
+++ main.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 export EC2_INI_PATH=inventory/ec2.ini
+# read input parameters
+vflag=""
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -v) vflag="-vvvv";;
+    -e) env="$2"; shift;;
+    -h)
+        echo >&2 "usage: $0 -e environment [-v]"
+        exit 1;;
+     *) break;; # terminate while loop
+  esac
+  shift
+done
 # create bastion VPC and autoscale groups
 # && setup bastion machines: separate playbook requires to refresh dynamic EC2 inventory
-ansible-playbook --extra-vars "environ=$1" main_setup.yaml $2 \
-&& ansible-playbook --extra-vars "environ=$1" main_config.yaml $2
+ansible-playbook --extra-vars "environ=$env" main_setup.yaml $vflag \
+&& ansible-playbook --extra-vars "environ=$env" main_config.yaml $vflag
